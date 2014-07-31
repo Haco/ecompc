e663a05dae7ce50f75dadc652ca6784a4f1bf5e3
Scheidler, Nicolas <Nicolas.Scheidler@ecom-ex.com>
2014-07-31 08:59:39 +0200

    Fixed height of checkbox via jQuery resize

---
+++ Resources/Public/JS/app.js
@@ -0,0 +1,10 @@
+(function($) {
+	function equalizeHeight() {
+		$('#tx-ecompc-canvas .ecom-configurator-package-select').each(function() {
+			$(this).siblings('.ecom-configurator-package-state').height($(this).outerHeight());
+		});
+	}equalizeHeight();
+	$(window).resize(function() {
+		equalizeHeight();
+	});
+})(jQuery);
